5376c4310b5e6a6f28d0427357b7e7af22315980
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-10 16:08:47 -0800

    initial commit

---
+++ js-oop-mini-project/event.js
@@ -0,0 +1,32 @@
+class Event {
+  constructor(name,description){
+    this.name = name;
+    this.description = description;
+    this.availableTickets = []; 
+  }
+}
+const eventObj1 = new Event(
+  'KLOS Golden Gala',
+  'An evening with hollywood vampires'
+);
+const eventObj2 = new Event(
+  'Skillet & Sevendust', 
+  'Victorious war tour'
+);
+const eventObj3 = new Event(
+  'Jenny Lewis', 
+  'On the line tour 2019'
+);
+
+const eventArray = new Array();
+eventArray.push(eventObj1, eventObj2, eventObj3);
+console.log(eventArray);
+
+$(document).ready(function () {
+  let html = '';
+  $.each(eventArray, function (index, item) {
+    html += `<li>${item.name} - ${item.description}</li>`;
+  });
+  // insert final html into #event...
+  $('#event').html(html);
+});
