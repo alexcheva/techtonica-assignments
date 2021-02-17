c969d9b1df64c45ec280e4f9dd8687d777af72b7
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-16 17:49:37 -0800

    fix update event

--- eventonica/app.js
+++ eventonica/app.js
@@ -5,30 +5,22 @@ class Eventonica {
     new Event(name, category, location, date, time, price);
   }
 
-  updateEvent(id, name, category, location, date, time, price) {
+  updateEvent(id, property, change) {
     // Update existing Event
-    Event.all = Event.all.map(event => {
-      if(event.id === id){
-        event.name = name;
-        event.category = category;
-        event.location = location;
-        event.date = date;
-        event.time = time;
-        event.price = price;
-      }
-    });
+    let index = Event.all.map(event => event.id).indexOf(id);
+    Event.all[index][property] = change;
   }
 
   deleteEvent(id) {
     // Deletes Event
     //find the event with .id === id
     //remove it from the Event.all
-    for(let i=0; i<Event.all.length; i++){
-      if(Event.all[i].id === id){
-        Event.all.splice(i,1);
-      }
-    }
-    // Event.all = Event.all.filter(event => event.id !== id);
+    // for(let i=0; i<Event.all.length; i++){
+    //   if(Event.all[i].id === id){
+    //     Event.all.splice(i,1);
+    //   }
+    // }
+    Event.all = Event.all.filter(event => event.id !== id);
   }
 
   findEventsByDate(date) {
@@ -115,13 +107,14 @@ class User {
 //make events
 let eventonica = new Eventonica();
 eventonica.addEvent("HIM", "concert", "UCT", new Date('12 May 2021'), "12:00 PM", 55);
-console.log(Event.all)
+// console.log(Event.all)
 // Event.all[0].date.setHours(5);
 // Event.all[0].date.setMinutes(11);
 // Event.all[0].date.setUTCHours(12,31,01)
 
 
 eventonica.addEvent("POD", "concert", "UCT", new Date('12 July 2021'), "18:00 PM", 55);
-//DOES NOT WORK
-//eventonica.updateEvent(100,"HIM", "concert", "UCT", new Date('12 May 2021'), "18:00 PM", 55);
+//console.log(Event.all)
+eventonica.updateEvent(100, "time", "18:00 PM");
+//eventonica.deleteEvent(101);
 console.log(Event.all)
