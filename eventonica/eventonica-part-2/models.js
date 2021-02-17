250f79e543a010320d5039dbadbbc111042f8657
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-17 12:18:32 -0800

    add models and fields

--- eventonica/eventonica-part-2/models.js
+++ eventonica/eventonica-part-2/models.js
@@ -2,25 +2,93 @@
  * Paste your code from Part 1 into this file but keep the module check at the very end.
  * (The following is a just a small subset for the Add Event form on the example page to work)
  */
+class Eventonica {
+  
+  addEvent(name, category, location, date, time, price) {
+    // Adds a new Event
+    new Event(name, category, location, date, time, price);
+  }
+
+  updateEvent(id, property, change) {
+    // Update existing Event
+    let index = Event.all.map(event => event.id).indexOf(id);
+    Event.all[index][property] = change;
+  }
+
+  deleteEvent(id) {
+    // Deletes Event
+    Event.all = Event.all.filter(event => event.id !== id);
+  }
+
+  findEventsByDate(date) {
+    // Return items in Event.all with a specified date
+    return Event.all.filter(event=>event.date === date);
+  }
+
+  findEventsbyCategory(category) {
+    // Return items in Event.all with a specified category
+    return Event.all.filter(event=>event.category === category);
+  }
+
+  addUser(username, email, firstName, lastName) {
+    // Adds a new User
+    new User(username, email, firstName, lastName);
+  }
+
+  updateUser(id, property, change) {
+    // Update existing User
+    let index = User.all.map(user => user.id).indexOf(id);
+    User.all[index][property] = change;
+  }
+
+  deleteUser(id) {
+    // Deletes User
+    User.all = User.all.filter(user => user.id !== id);
+  }
+}
+
 class Event {
   static all = [];
   static _nextId = 100;
 
-  constructor(name) {
-    this.name = name;
+  constructor(name, category, location, date, time, price) {
     this.id = Event._nextId++;
+    this.name = name;
+    this.category = category;
+    this.location = location;
+    this.date = date;
+    this.time = time;
+    this.price = price;
+    // decide what properties are required
     Event.all.push(this); // keep track of all created instances
   }
-}
 
-class User {}
+  static findByDate(date) {
+    //find which events in array all have .date === date
+    return Event.all.filter(event => event.date === date);
+  }
 
-class Eventonica {
-  addEvent(name) {
-    return new Event(name);
+  static findByCategory(category) {
+    //find which events in array all have .category === category
+    return Event.all.filter(event => event.category === category);
   }
 }
 
+class User {
+  static all = [];
+  static _nextId = 200;
+  static favoriteEvents = [];
+
+  constructor(username, email, firstName, lastName) {
+    this.id = User._nextId++;
+    // decide what properties are required on an instance
+    this.username = username;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    User.all.push(this); // keep track of all created instances
+  }
+}
 if (typeof module !== "undefined") {
   module.exports = { Eventonica, User, Event };
 }
