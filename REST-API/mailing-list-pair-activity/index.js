41e1a71df56bf042ffce939de07bae5c739a7cc7
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-03 21:47:55 -0800

    add put and delete, rearrange

--- REST-API/mailing-list-pair-activity/index.js
+++ REST-API/mailing-list-pair-activity/index.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
-
-
-// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
-
-// add some fake data
-
 app.get('/lists', (req, res) => {
   const listsArray = Array.from(lists.keys()); // Why is this like this? Try it out in your console.
   res.send(listsArray);
@@ -25,11 +19,40 @@ app.route('/lists/:name').get((req,res)=>{
   res.status(status).send(response);
 });
 
+app.delete('/lists/:name', (req, res) => {
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  let newLists = lists.filter((list) => {
+      return list;
+  });
+
+  status = 200;
+  response = newLists;
+  res.status(status).send(response);
+});
+
+app.put('/lists/:name', (req, res) => {
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+      let newList = {};
+  lists.forEach((list) => {
+      newList = req.body;
+  });
+
+  status = 200;
+  response = newList;
+  res.status(status).send(response);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
 
 //FAKE DATA:
+// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
 const lists = new Map();
 
 const staffList = {
