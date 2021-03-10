7447b1f301802f3024edeecdae1bc9311675f705
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-10 10:04:25 -0800

    use component as a arrow function

--- React/scoreboard/app.js
+++ React/scoreboard/app.js
@@ -1,11 +1,9 @@
-function Header() {
-  return (
-    <header>
-    <h1>Scoreboard</h1>
-    <span className="stats">Players: 1</span>
-    </header>
-  );
-}
+const Header = () => 
+  <header>
+  <h1>Scoreboard</h1>
+  <span className="stats">Players: 1</span>
+  </header>;
+
  //console.log(title);
  ReactDOM.render(
    <Header />,
