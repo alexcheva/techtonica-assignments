76e3ac3249bba8c4234c6691d107519f6b72b057
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-10 15:04:21 -0800

    add component and child components

--- React/scoreboard/app.js
+++ React/scoreboard/app.js
@@ -1,12 +1,33 @@
-const Header = () => 
-  <header>
-  <h1>Scoreboard</h1>
-  <span className="stats">Players: 1</span>
-  </header>;
-
+const Header = () => {
+  return (
+    <header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players: 1</span>
+    </header>
+  );
+}
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Alex
+      </span>
+      <Counter />
+    </div>
+  );
+}
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
  //console.log(title);
  ReactDOM.render(
-   <Header />,
+   <Player />,
    document.getElementById('root')
 
  );
