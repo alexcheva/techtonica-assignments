40eee59c349526d69d7c0da43f13ef88e96bc511
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-15 15:34:13 -0700

    make external css in react

--- React/react-css/src/App.js
+++ React/react-css/src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Style.css';
 function App() {
   return (
-    <div style={ { margin:"10px 0", backgroundColor:"orange", display:"flex"}} >
-    {/* Start Parent Div */}
-    <div>
-    {/* Start of Child Divs */}
-    <div style={{flexBasis:"25%",padding:"10px",backgroundColor:"green"}}>Child Div 1</div>
-    <div style={{flexBasis:"25%",padding:"10px",backgroundColor:"green"}}>Child Div 2</div>
-    <div style={{flexBasis:"25%",padding:"10px",backgroundColor:"green"}}>Child Div 3</div>
-    {/* End of Child Divs */}
+    <div className="App">
+    {/* Start of Parent Div */}
+    <div className="parent">
+      {/* Start of Child Divs */}
+      <div className="child1">Child 1</div>
+      <div className="child2">Child 2</div>
+      <div className="child3">Child 3</div>
     </div>
     {/* End of Parent Div */}
-    </div>
+  </div>
   );
 }
 
